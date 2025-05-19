8386bf4864be341d382d25b065dd2ac0ff1b3b85
Tomás Atanasoff <tomyatana@gmail.com>
2025-05-19 09:04:59 -0300

    Adding checkpoint gen

---
+++ Assets/Scripts/RoadCheckpointGen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadCheckpointGen : MonoBehaviour
+{
+    [SerializeField]
+    string CheckpointName = "Checkpoint";
+    [SerializeField]
+    GameObject CheckpointPrefab;
+
+    void Awake() {
+        foreach (Transform child in transform) {
+            if(child.name != CheckpointName) continue;
+            Instantiate(CheckpointPrefab, child.position, Quaternion.identity);
+            Destroy(child.gameObject);
+        }
+    }
+}
